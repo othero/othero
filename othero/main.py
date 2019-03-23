18dadcf91adda80e098b7c86957a564e5bbcc284
kanatatsu64 <kanatatsu64@gmail.com>
2019-03-24 01:16:16 +0900

    Add othero/main.py, which will be the entry point.

---
+++ othero/main.py
@@ -0,0 +1,10 @@
+# set the search path
+#  import sys
+#  import os
+#  cwd = os.getcwd() # current working directory
+#  sys.path.append(cwd) # cwd is automatically added
+
+from othero.base import core
+
+if __name__ == "__main__":
+    pass
