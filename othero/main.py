c6999178adcea9ee11c4d4abdd5efde7c1d33497
kanatatsu64 <kanatatsu64@gmail.com>
2019-03-24 13:51:39 +0900

    Remove unnecessary commented import sentenses.

--- othero/main.py
+++ othero/main.py
@@ -3,12 +3,6 @@
 # This source code is licensed under the MIT License found in
 # the LICENSE file in the root directory of this source tree.
 
-# set the search path
-#  import sys
-#  import os
-#  cwd = os.getcwd() # current working directory
-#  sys.path.append(cwd) # cwd is automatically added
-
 from othero.base import core
 
 if __name__ == "__main__":
