03b84b19d704770e61d061b59eb123deacdf117c
kanatatsu64 <kanatatsu64@gmail.com>
2019-03-24 13:52:28 +0900

    Fix an incorrect module name.

--- othero/main.py
+++ othero/main.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT License found in
 # the LICENSE file in the root directory of this source tree.
 
-from othero.base import core
+from othero import core
 
 if __name__ == "__main__":
     pass
