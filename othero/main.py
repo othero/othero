1fa51c84a1b19ab4fb497858ba9b60c64892e3df
kanatatsu64 <kanatatsu64@gmail.com>
2019-03-24 13:50:35 +0900

    Add license notation to othero/main.py.

--- othero/main.py
+++ othero/main.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2019 Tanaka Tatsuya and contributors
+#
+# This source code is licensed under the MIT License found in
+# the LICENSE file in the root directory of this source tree.
+
 # set the search path
 #  import sys
 #  import os
