25fc30609cef99ffaceb8535e5108f97ebe7d80b
kanatatsu64 <kanatatsu64@gmail.com>
2019-04-05 22:49:26 +0900

    Add: calc_all_next_sogs function and its tests.
    Add calc_all_next_sogs function in othero/search/libfws.py.

--- othero/search/libfws.py
+++ othero/search/libfws.py
@@ -5,8 +5,11 @@
 # This source code is licensed under the MIT License found in
 # the LICENSE file in the root directory of this source tree.
 
-from othero.core import libboard, libtypes
+import enum
+
+from othero.core import libboard, libdisk, libtypes
 from othero.rule import libforward
+from othero.search import utils
 
 def get_available_positions(sog, disk):
     """
@@ -28,3 +31,23 @@ def get_available_positions(sog, disk):
     blank_poss = libboard.get_positions_in_sos(sog, libtypes.SOS.BLANK)
     return [pos for pos in blank_poss \
                 if libforward.is_sos_change_valid(sog, pos, sos)]
+
+def calc_all_next_sogs(sog, disk):
+    """
+    Restore sos at the <pos> to <sos> and calculate the resulting
+    possible sogs according to the osero rule.
+
+    Args: 
+        sog [[othero.core.libtypes.SOS]]:
+
+        disk othero.core.libtypes.Disk:
+            A disk to be put.
+
+    Returns:
+        [[[othero.core.libtypes.SOS]]]:
+            A list of possible sogs in the next turn. If there is no
+            position at which <disk> can be put, the list is empty.
+    """
+    sos = libtypes.Disk.toSOS(disk)
+    return [libforward.calc_sog_after_sos_changed(sog, pos, sos)
+                for pos in get_available_positions(sog, disk)]
--- test/search/test_libfws.py
+++ test/search/test_libfws.py
@@ -23,3 +23,40 @@ class TestSearchFws(unittest.TestCase):
             libfws.get_available_positions(self.__SOG, libtypes.Disk.DARK),
             [(0, 2), (1, 2), (2, 2)]
         )
+
+    def test_calc_all_next_sogs1(self):
+        def is_equal(x, y):
+            return all([a in y for a in x]) \
+                    and all([a in x for a in y])
+
+        expect = [
+            [
+                [libtypes.SOS.BLANK, libtypes.SOS.DARK , libtypes.SOS.DARK , libtypes.SOS.DARK ],
+                [libtypes.SOS.DARK , libtypes.SOS.DARK , libtypes.SOS.BLANK, libtypes.SOS.BLANK],
+                [libtypes.SOS.DARK , libtypes.SOS.LIGHT, libtypes.SOS.BLANK, libtypes.SOS.BLANK],
+                [libtypes.SOS.DARK , libtypes.SOS.DARK , libtypes.SOS.DARK , libtypes.SOS.BLANK]
+            ],
+            [
+                [libtypes.SOS.BLANK, libtypes.SOS.DARK , libtypes.SOS.BLANK, libtypes.SOS.DARK ],
+                [libtypes.SOS.DARK , libtypes.SOS.DARK , libtypes.SOS.DARK , libtypes.SOS.BLANK],
+                [libtypes.SOS.DARK , libtypes.SOS.DARK , libtypes.SOS.BLANK, libtypes.SOS.BLANK],
+                [libtypes.SOS.DARK , libtypes.SOS.DARK , libtypes.SOS.DARK , libtypes.SOS.BLANK] 
+            ],
+            [
+                [libtypes.SOS.BLANK, libtypes.SOS.DARK , libtypes.SOS.BLANK, libtypes.SOS.DARK ],
+                [libtypes.SOS.DARK , libtypes.SOS.LIGHT, libtypes.SOS.BLANK, libtypes.SOS.BLANK],
+                [libtypes.SOS.DARK , libtypes.SOS.DARK , libtypes.SOS.DARK , libtypes.SOS.BLANK],
+                [libtypes.SOS.DARK , libtypes.SOS.DARK , libtypes.SOS.DARK , libtypes.SOS.BLANK]
+            ],
+        ]
+
+        self.assertTrue(is_equal(
+            libfws.calc_all_next_sogs(self.__SOG, libtypes.Disk.DARK),
+            expect
+        ))
+
+    def test_calc_all_next_sogs2(self):
+        self.assertEqual(
+            libfws.calc_all_next_sogs(self.__SOG, libtypes.Disk.LIGHT),
+            [] 
+        )
