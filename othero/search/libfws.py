4e0c778a105cfbd1db748727789e057d63a831c4
kanatatsu64 <kanatatsu64@gmail.com>
2019-04-05 22:54:00 +0900

    Add: Forward search data structures and their tests.
    Add FwsNode and FwsTree in othero/search/libfws.py.

--- othero/search/libfws.py
+++ othero/search/libfws.py
@@ -51,3 +51,81 @@ def calc_all_next_sogs(sog, disk):
     sos = libtypes.Disk.toSOS(disk)
     return [libforward.calc_sog_after_sos_changed(sog, pos, sos)
                 for pos in get_available_positions(sog, disk)]
+
+class FwsNode:
+    class ReservedKeys(enum.Enum):
+        IS_DTW = "isDtw"
+
+    @staticmethod
+    def create(tree, prev_node, cur_sog, prev_disk):
+        node = FwsNode()
+        node.setParams(tree, prev_node, cur_sog, prev_disk)
+        node.initialize()
+        return node
+
+    @staticmethod
+    def delete(tree, node):
+        pass
+
+    def setParams(self, tree, prev_node, cur_sog, prev_disk):
+        self.cur_sog = cur_sog
+        self.next_nodes = None 
+        self.prev_node = prev_node
+        self.next_disk = None
+        self.prev_disk = prev_disk
+        self.isLeaf = None
+        self.__next_sogs = None
+        self.tree = tree
+        self.__mark = {}
+
+    def initialize(self):
+        self.__search_nexts()
+
+    def expand(self):
+        if self.next_nodes == None:
+            if self.__next_sogs == None:
+                self.__search_nexts()
+            self.next_nodes = [
+                self.create(self.tree, self, sog, self.next_disk)
+                for sog in self.__next_sogs
+            ]
+
+    def shrink(self):
+        for node in self.next_nodes:
+            self.delete(self.tree, node)
+        self.next_nodes = None
+        self.__next_sogs = None
+
+    def addMark(self, key, value):
+        self.__mark[key] = value
+
+    def getMark(self, key):
+        if key not in self.__mark.keys():
+            return None
+        return self.__mark[key]
+
+    def delMark(self, key):
+        if key in self.__mark.keys():
+            del self.__mark[key]
+
+    def __search_nexts(self):
+        next_disk = libdisk.next_disk(self.prev_disk)
+        for _ in range(len(libtypes.Disk)):
+            next_sogs = calc_all_next_sogs(self.cur_sog, next_disk)
+            if len(next_sogs) == 0:
+                next_disk = libdisk.next_disk(next_disk)
+                continue
+            break
+        else:
+            self.isLeaf = True
+            self.next_disk = None
+            self.__next_sogs = []
+            return
+        
+        self.isLeaf = False
+        self.next_disk = next_disk
+        self.__next_sogs = next_sogs
+
+class FwsTree:
+    def __init__(self, sog, first_disk=libtypes.Disk.DARK):
+        self.root = FwsNode.create(self, None, sog, libdisk.prev_disk(first_disk))
--- test/search/test_libfws.py
+++ test/search/test_libfws.py
@@ -60,3 +60,82 @@ class TestSearchFws(unittest.TestCase):
             libfws.calc_all_next_sogs(self.__SOG, libtypes.Disk.LIGHT),
             [] 
         )
+
+class TestSearchFwsFwsNode(unittest.TestCase):
+    __SOG = [
+        [libtypes.SOS.BLANK, libtypes.SOS.DARK , libtypes.SOS.BLANK, libtypes.SOS.DARK ],
+        [libtypes.SOS.DARK , libtypes.SOS.LIGHT, libtypes.SOS.BLANK, libtypes.SOS.BLANK],
+        [libtypes.SOS.DARK , libtypes.SOS.LIGHT, libtypes.SOS.BLANK, libtypes.SOS.BLANK],
+        [libtypes.SOS.DARK , libtypes.SOS.DARK , libtypes.SOS.DARK , libtypes.SOS.BLANK]
+    ]
+
+    __TREE = libfws.FwsTree(__SOG)
+
+    def test_create(self):
+        node = libfws.FwsNode.create(self.__TREE, self.__TREE.root, self.__SOG, libtypes.Disk.LIGHT)
+        self.assertEqual(node.cur_sog, self.__SOG)
+        self.assertEqual(node.prev_node, self.__TREE.root)
+        self.assertEqual(node.prev_disk, libtypes.Disk.LIGHT)
+        self.assertEqual(node.tree, self.__TREE)
+        self.assertEqual(node.next_disk, libtypes.Disk.DARK)
+        self.assertFalse(node.isLeaf)
+
+        self.assertIsNone(node.next_nodes)
+
+    def test_delete(self):
+        pass
+
+    def test_setParams(self):
+        node = libfws.FwsNode()
+        node.setParams(self.__TREE, self.__TREE.root, self.__SOG, libtypes.Disk.LIGHT)
+        self.assertEqual(node.cur_sog, self.__SOG)
+        self.assertEqual(node.prev_node, self.__TREE.root)
+        self.assertEqual(node.prev_disk, libtypes.Disk.LIGHT)
+        self.assertEqual(node.tree, self.__TREE)
+
+        self.assertIsNone(node.next_nodes)
+        self.assertIsNone(node.next_disk)
+        self.assertIsNone(node.isLeaf)
+
+    def test_initialize(self):
+        node = libfws.FwsNode()
+        node.setParams(self.__TREE, self.__TREE.root, self.__SOG, libtypes.Disk.LIGHT)
+        node.initialize() 
+        self.assertEqual(node.next_disk, libtypes.Disk.DARK)
+        self.assertIsNotNone(node.isLeaf)
+
+    def test_expand(self):
+        def is_equal(x, y):
+            return all([a in y for a in x]) \
+                    and all([a in x for a in y])
+
+        node = libfws.FwsNode.create(self.__TREE, self.__TREE.root, self.__SOG, libtypes.Disk.LIGHT)
+        node.expand()
+        self.assertIsNotNone(node.next_nodes)
+        self.assertTrue(is_equal(
+            list(map(lambda nd: nd.cur_sog, node.next_nodes)),
+            libfws.calc_all_next_sogs(node.cur_sog, node.next_disk)
+        ))
+
+    def test_shrink(self):
+        node = libfws.FwsNode.create(self.__TREE, self.__TREE.root, self.__SOG, libtypes.Disk.LIGHT)
+        node.expand()
+        node.shrink()
+        self.assertIsNone(node.next_nodes)
+        self.assertEqual(node.next_disk, libtypes.Disk.DARK) 
+        self.assertFalse(node.isLeaf)
+
+    def test_getMark(self):
+        node = libfws.FwsNode.create(self.__TREE, self.__TREE.root, self.__SOG, libtypes.Disk.LIGHT)
+        self.assertIsNone(node.getMark("key"))
+
+    def test_addMark__getMark(self):
+        node = libfws.FwsNode.create(self.__TREE, self.__TREE.root, self.__SOG, libtypes.Disk.LIGHT)
+        node.addMark("key", "value")
+        self.assertEqual(node.getMark("key"), "value")
+
+    def test_delMark(self):
+        node = libfws.FwsNode.create(self.__TREE, self.__TREE.root, self.__SOG, libtypes.Disk.LIGHT)
+        node.addMark("key", "value")
+        node.delMark("key")
+        self.assertIsNone(node.getMark("key"))
