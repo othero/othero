2b3fd1a9fb17ea5b8d4dd5dfa2e229c0cd055381
kanatatsu64 <kanatatsu64@gmail.com>
2019-04-08 23:11:21 +0900

    Add: Stopwatch decorator.

--- othero/main.py
+++ othero/main.py
@@ -5,7 +5,24 @@
 # This source code is licensed under the MIT License found in
 # the LICENSE file in the root directory of this source tree.
 
-from othero import core
+import time
+
+from othero.core import libtypes
+
+from othero.core import libsog
+from othero.search import libfws
+
+def stopwatch(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        func(*args, **kwargs)
+        stop = time.time()
+        print(f"{stop-start} seconds")
+    return wrapper
+
+@stopwatch
+def main():
+    pass
 
 if __name__ == "__main__":
-    pass
+    main()
